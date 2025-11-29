2e7b68157ad0ec1520742c8e596aa6c9c255f44a
daldama00 <157993636+daldama00@users.noreply.github.com>
2025-11-29 14:31:15 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+
+int yyparse();
+
+int main() {
+    printf("RPN Calculator (lex/yacc)\n");
+    printf("Enter RPN expressions, press Enter to evaluate.\n");
+    printf("Ctrl+C to exit.\n\n");
+    yyparse();
+    return 0;
+}
 
