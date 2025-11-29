c29a6cc5cb47791e795d127966fe15a274483535
daldama00 <157993636+daldama00@users.noreply.github.com>
2025-11-29 14:30:33 -0600

    Update rpn.h

--- src/rpn.h
+++ src/rpn.h
@@ -1,1 +1,13 @@
+#ifndef RPN_H
+#define RPN_H
+
+typedef struct NodeList {
+    double value;
+    struct NodeList *next;
+} NodeList;
+
+NodeList* append(NodeList *list, double value);
+double evaluate_list(NodeList *list);
+
+#endif
 
